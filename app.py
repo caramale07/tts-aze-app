b9f767a2970d5bee8dea01a04917632643435275
Fatulla <fatulla2004@gmail.com>
2024-06-03 23:47:53 +0400

    added

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def main():
+    st.title("Azerbaijani Text-to-Speech (TTS) Demo")
+
+    st.write("## Input Text")
+    user_text = st.text_area("Enter the text you want to convert to speech:")
+
+    st.write("## Select Dialect")
+    dialects = ["Baku", "Ganja", "Shirvan", "Karabakh"]
+    selected_dialect = st.selectbox("Choose a dialect:", dialects)
+
+    st.write("## Select Voice")
+    gender = st.radio("Choose the gender of the voice:", ("Male", "Female"))
+
+    if st.button("Generate Speech"):
+        st.write("Generating speech...  (This may take a few seconds")
+
+        # Placeholder for generated audio URL
+        audio_url = "ReelAudio-29983.mp3"
+        
+        st.audio(audio_url, format="audio/mp3")
+        st.download_button(label="Download Audio", data=open(audio_url, "rb"), file_name="generated_audio.mp3")
+
+if __name__ == "__main__":
+    main()
